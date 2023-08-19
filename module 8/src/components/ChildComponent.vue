dad05c012f950482ef81a94f9e37dece0b7d43f6
Sheikh Raihan Al Saleh <114013600+Sheikh-Raihan-Al-Saleh@users.noreply.github.com>
2023-08-20 00:36:46 +0600

    Module 8 Assignment Completed

---
+++ module 8/src/components/ChildComponent.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import { ref, defineEmits, defineProps } from 'vue';
+
+    defineProps(['message']);
+    const emit = defineEmits(['messageEmitted']);
+
+    const emitMessage = () => {
+        const customMessage = "Hey How are you?";
+        emit('messageEmitted', customMessage);
+    };
+
+</script>
+  
+
+<template>
+    <div class="p-4 border border-gray-300 rounded-md mt-4">
+        <h2 class="text-lg font-semibold">Child Component</h2>
+        <p class="mt-2">Message: {{ message }}</p>
+        <button @click="emitMessage" class="mt-2 bg-blue-900 text-white px-4 py-2 rounded">Click Me</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+  
---
+++ module 8/src/components/ParentComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import ChildComponent from './ChildComponent.vue';
+    import { ref } from 'vue';
+
+    const message = ref("Hello There!!!");
+
+    const handleMessageEmitted = (emittedMessage) => {
+    message.value = emittedMessage;
+    };
+
+</script>
+
+<template>
+    <div class="p-4">
+        <h1 class="text-2xl font-semibold">Parent Component</h1>
+        <p class="mt-2">Message: {{ message }}</p>
+        <ChildComponent :message="message" @messageEmitted="handleMessageEmitted"/>
+    </div>
+</template>
+  
+<style scoped>
+
+</style>
+  
